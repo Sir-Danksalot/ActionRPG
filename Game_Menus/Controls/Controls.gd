dc58edf6264a4502feaf7395bad6159718e4931b
Areeb SM <areeb.sm@gmail.com>
2020-09-12 22:42:38 +0530

    Implemented Pause menu.

--- Game_Menus/Controls/Controls.gd
+++ Game_Menus/Controls/Controls.gd
@@ -1,4 +1,4 @@
 extends Control
 
-func _on_Button_pressed():
+func _on_Button_pressed() -> void:
 	get_tree().change_scene("res://Game_Menus/Main_Menu/Main_Menu.tscn") # Replace with function body.
--- Game_Menus/Main_Menu/Main_Menu.gd
+++ Game_Menus/Main_Menu/Main_Menu.gd
@@ -1,9 +1,9 @@
 extends MarginContainer
 
 
-func _on_Controls_pressed():
+func _on_Controls_pressed() -> void:
 	get_tree().change_scene("res://Game_Menus/Controls/Controls.tscn") # Replace with function body.
 
 
-func _on_NewGame_pressed():
+func _on_NewGame_pressed() -> void:
 	get_tree().change_scene("res://Environment/World/World.tscn") # Replace with function body.
