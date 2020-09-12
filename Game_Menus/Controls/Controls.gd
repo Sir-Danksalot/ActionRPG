bdaccd87350259d0261ad4c4349ab53b063debba
Areeb SM <areeb.sm@gmail.com>
2020-09-12 21:15:39 +0530

    Added Controls menu and connected new game and controls buttons.

---
+++ Game_Menus/Controls/Controls.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Game_Menus/Main_Menu/Main_Menu.tscn") # Replace with function body.
---
+++ Game_Menus/Main_Menu/Main_Menu.gd
@@ -0,0 +1,9 @@
+extends MarginContainer
+
+
+func _on_Controls_pressed():
+	get_tree().change_scene("res://Game_Menus/Controls/Controls.tscn") # Replace with function body.
+
+
+func _on_NewGame_pressed():
+	get_tree().change_scene("res://Environment/World/World.tscn") # Replace with function body.
