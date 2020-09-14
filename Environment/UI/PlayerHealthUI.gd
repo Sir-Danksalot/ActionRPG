c6797ed8dcba826b21e822de7f5de8a2b50ab46f
Areeb SM <areeb.sm@gmail.com>
2020-09-14 10:03:44 +0530

    Forgot to add some files in previous commit

--- Environment/UI/PlayerHealthUI.gd
+++ Environment/UI/PlayerHealthUI.gd
@@ -1,16 +1,16 @@
 extends Control
 
-const height:int = 11
+const height:int = 44
 # Each heart is 15 wide
 onready var maxHealth = $MaxHealth
 onready var currentHealth = $CurrentHealth
 # Called when the node enters the scene tree for the first time.
 
 func setMaxHealth(hp:float):
-	maxHealth.set("rect_size",Vector2(15 * hp,height))
+	maxHealth.set("rect_size",Vector2(60 * hp,height))
 
 func setCurrentHealth(hp:float):
-	currentHealth.set("rect_size",Vector2(15 * hp,height))
+	currentHealth.set("rect_size",Vector2(60 * hp,height))
 
 func _on_health_changed(health:float):
 	setCurrentHealth(health)
